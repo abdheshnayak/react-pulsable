b9ec2ad87b21ff98508f6180bad7b72ac35dfe2f
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-11-03 19:21:51 +0530

    :art: Added transparency in image placeholder

--- src/image-placeholder.ts
+++ src/image-placeholder.ts
@@ -10,10 +10,8 @@ export const iPlaceholder = `
       <path
         d="m118.507 133.514-34.249 34.249-15.968-15.968-41.938 41.937H178.726z"
         opacity=".675"
-        fill="#fff"
       />
-      <circle cx="58.217" cy="108.555" r="11.773" opacity=".675" fill="#fff" />
-      <path fill="none" d="M26.111 77.634h152.614v116.099H26.111z" />
+      <circle cx="58.217" cy="108.555" r="11.773" opacity=".675" />
     </g>
   </svg>
 `;
